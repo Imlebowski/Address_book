e4c6f71d40e0a209d9581fba989381fb655ba687
Venkat Krishna R <Imlebowski@users.noreply.github.com>
2017-08-07 17:11:19 +0530

    Update check_storage_format.c

--- fun/check_storage_format.c
+++ fun/check_storage_format.c
@@ -4,15 +4,12 @@
 
 Status check_storage_format(Address_book *addbk)
 {
+	fseek(addbk->fname_ptr, 0, SEEK_SET);
+	fread(&addbk->ch, 1, 1, addbk->fname_ptr);
 
-
-		fseek(addbk->fname_ptr, 0, SEEK_SET);
-fread(&addbk->ch, 1, 1, addbk->fname_ptr);
-
-if (addbk->ch != '*')
-{
-			printf(KRED"\n\n\t\t\tTHIS FILE CANNOT BE USED!!! STORAGE FORMAT MISSMATCH.\n\n"KNRM);
-			return a_failure;
-
-}
+	if (addbk->ch != '*')
+	{
+		printf(KRED"\n\n\t\t\tTHIS FILE CANNOT BE USED!!! STORAGE FORMAT MISSMATCH.\n\n"KNRM);
+		return a_failure;
+	}
 }
